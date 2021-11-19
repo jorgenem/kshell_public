2d1f0446bd9a04e39e1461bb884045f38840cd65
Jon Dahl <jon@dira.no>
2021-11-19 15:17:52 +0100

    Improve docstring and variable naming

--- bin/collect_logs.py
+++ bin/collect_logs.py
@@ -2,18 +2,33 @@
 # ./collect_logs.py log_foo.txt log_bar.txt ...
 #
 import sys
-from math import *
+from typing import List
+# from math import *
+from math import pi
 
 #wu_threshold = 1.0 # threshold to show in W.u.
 wu_threshold = -0.001
 
-e_data = {}
+e_data = {}     # e_data[energy] = (log filename, spin, parity, eigenstate number, tt).
 n_jnp = {}
 
 e_gs = 0.0
 
 
-def i2prty(i):
+def i2prty(i: int) -> str:
+    """
+    Convert 1 to '+' and -1 to '-'.
+
+    Parameters
+    ----------
+    i : int
+        Parity in integer representation.
+
+    Returns
+    -------
+    : str
+        The string representation of the parity.
+    """
     if i == 1: return '+'
     else: return '-'
 
@@ -35,8 +50,23 @@ def weisskopf_unit(asc, mass):
         raise 'weisskopf error'
     return  wu, unit
     
-def read_file_ene(fn):
-    fp = open(fn, 'r')
+def read_file_ene(filename: str):
+    """
+    Extract the energy, spin, and parity for each eigenstate and arrange
+    the data in a dictionary, e_data, where the keys are the energies
+    and the values are tuples of
+    (log filename, spin, parity, eigenstate number, tt).
+
+    The transition logs will not be read by this function as they do not
+    contain the energy level information. Only the KSHELL logs will be
+    read.
+
+    Parameters
+    ----------
+    filename : str
+        The log filename.
+    """
+    fp = open(filename, 'r')
     while True:
         line = fp.readline()
         if not line: break
@@ -46,17 +76,29 @@ def read_file_ene(fn):
             line = fp.readline()
             if not line: break
             if line[6:10] == '<H>:':
-                n_eig= int(line[:5])
-                ene  = float(line[11:22])
-                mtot = int(line[45:48])
-                prty = int(line[57:59])
-                prty = i2prty(prty)
-                while ene in e_data: ene += 0.000001
+                """
+                Example:
+                -------------------------------------------------
+                   1  <H>:  -391.71288  <JJ>:    -0.00000  J:  0/2  prty -1     <-- this line will be read
+                    <Hcm>:     0.00022  <TT>:     6.00000  T:  4/2              <-- this line will be read
+                 <p Nj>  5.944  3.678  1.489  3.267  0.123  0.456  0.043        <-- this line will be skipped
+                 <n Nj>  5.993  3.902  1.994  5.355  0.546  0.896  0.314        <-- this line will be skipped
+                 hw:  1:1.000                                                   <-- this line will be skipped
+                -------------------------------------------------
+                NOTE: All this substring indexing seems to be easily
+                replaceable with a simple split!
+                """
+                n_eig = int(line[:5])       # Eigenvalue number. 1, 2, 3, ...
+                energy = float(line[11:22])
+                spin = int(line[45:48])     # 2*spin actually.
+                parity = int(line[57:59])
+                parity = i2prty(parity)
+                while energy in e_data: energy += 0.000001  # NOTE: To separate energies close together? Else keys may be identical!
                 while True:
                     line = fp.readline()
                     if line[42:45] != ' T:': continue
                     tt = int(line[45:48])
-                    e_data[ ene ] = (fn, mtot, prty, n_eig, tt)
+                    e_data[ energy ] = (filename, spin, parity, n_eig, tt)
                     break
     fp.close()
 
@@ -68,10 +110,10 @@ def str_JJ(jj):
     else:
         return str(jj) + '/2'
         
-def read_file_tran(fn, asc):
+def read_file_tran(filename, asc):
     out_e = {}
     is_r = False
-    fp = open(fn, 'r')
+    fp = open(filename, 'r')
     fn_l, fn_r = 'a', 'b'
     mass_save = 0
     while True:
@@ -126,11 +168,11 @@ def read_file_tran(fn, asc):
                 """
                 continue
             
-            j1 = int(line[:2])      # J final.
+            spin_final = int(line[:2])      # J final.
             n1 = int(line[3:7])
-            j2 = int(line[17:19])   # J initial.
+            spin_initial = int(line[17:19])   # J initial.
             n2 = int(line[20:24])
-            if j1 == j2 and n1 == n2: continue
+            if spin_final == spin_initial and n1 == n2: continue
             
             ex = float(line[34:42]) # Gamma energy.
             if is_diag and ex < 0.0: continue
@@ -148,8 +190,8 @@ def read_file_tran(fn, asc):
             wu1, wu2 = b1/wu, b2/wu
             if max(wu1, wu2) < wu_threshold: continue
 
-            n1 = n_jnp[ (j1, prty1, n1) ]
-            n2 = n_jnp[ (j2, prty2, n2) ]
+            n1 = n_jnp[ (spin_final, prty1, n1) ]
+            n2 = n_jnp[ (spin_initial, prty2, n2) ]
             stringformat \
                 = "%4s%c(%2d) %6.3f %4s%c(%2d) %6.3f %6.3f " \
                 + "%8.1f(%5.1f) %8.1f(%5.1f)\n"
@@ -159,51 +201,51 @@ def read_file_tran(fn, asc):
                 
             if ex > 0.0:
                 out = stringformat \
-                      % (str_JJ(j2), prty2, n2, E2, 
-                         str_JJ(j1), prty1, n1, E1, 
+                      % (str_JJ(spin_initial), prty2, n2, E2, 
+                         str_JJ(spin_final), prty1, n1, E1, 
                          ex,  b2, wu2, b1, wu1)
-                ky = E2 + E1 * 1e-5 + j2 *1.e-10 + n2*1.e-11 + j1*1.e-13 + n1*1.e-14
+                ky = E2 + E1 * 1e-5 + spin_initial *1.e-10 + n2*1.e-11 + spin_final*1.e-13 + n1*1.e-14
             else:
                 out = stringformat \
-                      % (str_JJ(j1), prty1, n1, E1, 
-                         str_JJ(j2), prty2, n2, E2, 
+                      % (str_JJ(spin_final), prty1, n1, E1, 
+                         str_JJ(spin_initial), prty2, n2, E2, 
                          -ex, b1, wu1, b2, wu2)
-                ky = E1 + E2 * 1e-5 + j1 *1.e-10 + n1*1.e-11 + j2*1.e-12 + n2*1.e-14
+                ky = E1 + E2 * 1e-5 + spin_final *1.e-10 + n1*1.e-11 + spin_initial*1.e-12 + n2*1.e-14
             out_e[ky] = out
     fp.close()
     return is_r, out_e, mass_save
 
-def main(fn_list):
+def main(filename_list: List):
     print("\n Energy levels")
-    for fn in fn_list:
-        read_file_ene(fn)
+    for filename in filename_list:
+        read_file_ene(filename)
 
     keys = e_data.keys()
     if len(keys) > 0:
         keys = sorted(keys)
         njp = {}
         for e in keys:
-            fn, mtot, prty, n_eig, tt = e_data[e]
+            filename, mtot, prty, n_eig, tt = e_data[e]
             mp = (mtot, prty)
             njp[mp] = njp.get(mp, 0) + 1
             n_jnp[ (mtot, prty, n_eig) ] = njp[mp]
-            e_data[e] = fn, mtot, prty, njp[mp], tt
+            e_data[e] = filename, mtot, prty, njp[mp], tt
 
         global e_gs
         e_gs = keys[0]
         print('\n    N    J prty N_Jp    T     E(MeV)  Ex(MeV)  log-file\n')
         for i, e in enumerate(keys):
-            fn, mtot, prty, n_eig, tt = e_data[e]
+            filename, mtot, prty, n_eig, tt = e_data[e]
             print("%5d %5s %1s %5d %5s %10.3f %8.3f  %s " \
-                % (i+1, str_JJ(mtot), prty, n_eig, str_JJ(tt), e, e-e_gs, fn))
+                % (i+1, str_JJ(mtot), prty, n_eig, str_JJ(tt), e, e-e_gs, filename))
         print()
 
 
     def print_transition(asc):
         is_show = False
         output_e = {}
-        for fn in sys.argv[1:]:
-            is_r, out_e, mass = read_file_tran(fn, asc)
+        for filename in sys.argv[1:]:
+            is_r, out_e, mass = read_file_tran(filename, asc)
             if is_r: is_show = is_r
             output_e.update( out_e )
         wu, unit = weisskopf_unit(asc, mass)
@@ -222,5 +264,6 @@ B(%s)  ( > %.1f W.u.)  mass = %d    1 W.u. = %.1f %s
 #    print_transition('E3')
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    # main(sys.argv[1:])
+    read_file_ene("log_V50_sdpf-mu_j0n.txt")
 
