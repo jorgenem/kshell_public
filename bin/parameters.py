fae9d42c733320b2e13cc1a64e97c22f67d46e98
Jon Dahl <jon@dira.no>
2022-03-11 15:36:42 +0100

    Hold common parameters in a separate file

---
+++ bin/parameters.py
@@ -0,0 +1,7 @@
+GS_FREE_PROTON = 5.585
+GS_FREE_NEUTRON = -3.826
+
+recommended_quenching_factors = {
+    "GCLSTsdpfsdgix5pn.snt": f"0.75*GS_FREE = {round(0.75*GS_FREE_PROTON, 3), round(0.75*GS_FREE_NEUTRON, 3)}",
+    "gs8.snt": f"0.75*GS_FREE = {round(0.75*GS_FREE_PROTON, 3), round(0.75*GS_FREE_NEUTRON, 3)}"
+}
