d9121ea3d67be53ca1f8e19d8efe2ab1d02196b7
Jon Dahl <jon@dira.no>
2022-04-22 15:03:26 +0200

    Fix a bunch of indentations (4 spaces)

--- src/lanczos.f90
+++ src/lanczos.f90
@@ -1,34 +1,31 @@
 module lanczos
-  use constant, only : kwf, kdim, maxchar
-  use wavefunction, only: type_vec_p
-  use lib_matrix, only: gaussian_random_mat, diagonalize, &
-       diagonalize_sym_tridiag
-  use model_space, only: myrank, nprocs, sum_rule, &
-       allocate_l_vec, deallocate_l_vec, print_max_l_vec, is_mpi
-  use bp_io, only: dump_snapshot_mpi_sf, load_snapshot_mpi_sf, &
-       set_dump_fname_mpi_sf
-  use class_stopwatch
-  implicit none
-  private
-  public :: lanczos_main, max_lanc_vec_doublej, &
-       read_vec, write_vec, set_lanczos_tmp_fn
-  ! for block lanczos
-  public :: is_save_tmp, fn_base_dump
-
-!  integer, parameter :: max_lanc_vec_doublej=10
-  integer, parameter :: max_lanc_vec_doublej=3
-
-  character(len=maxchar) :: fn_base_dump='tmp_snapshot_'
-  character(len=maxchar) :: fn_base_lv='tmp_lv_'
-  integer, parameter :: lun_lv=23
-  logical :: is_first_inner = .true., is_save_tmp = .true.
+    use constant, only : kwf, kdim, maxchar
+    use wavefunction, only: type_vec_p
+    use lib_matrix, only: gaussian_random_mat, diagonalize, &
+        diagonalize_sym_tridiag
+    use model_space, only: myrank, nprocs, sum_rule, &
+        allocate_l_vec, deallocate_l_vec, print_max_l_vec, is_mpi
+    use bp_io, only: dump_snapshot_mpi_sf, load_snapshot_mpi_sf, &
+        set_dump_fname_mpi_sf
+    use class_stopwatch
+    implicit none
+    private
+    public :: lanczos_main, max_lanc_vec_doublej, read_vec, write_vec, &
+        set_lanczos_tmp_fn
+    ! for block lanczos
+    public :: is_save_tmp, fn_base_dump
+    ! integer, parameter :: max_lanc_vec_doublej=10
+    integer, parameter :: max_lanc_vec_doublej=3
+    character(len=maxchar) :: fn_base_dump='tmp_snapshot_'
+    character(len=maxchar) :: fn_base_lv='tmp_lv_'
+    integer, parameter :: lun_lv=23
+    logical :: is_first_inner = .true., is_save_tmp = .true.
   
 contains
-
-  recursive subroutine lanczos_main(matvec, dotprod, ndim, eval, evec, &
-       matvec_jj, eval_jj, is_inner, is_load_snapshot, &
-       n_eig, tol, maxiter, n_restart_vec, max_lanc_vec, &
-       mode_lv_hdd)
+    recursive subroutine lanczos_main(&
+        matvec, dotprod, ndim, eval, evec, matvec_jj, eval_jj, is_inner, &
+        is_load_snapshot, n_eig, tol, maxiter, n_restart_vec, max_lanc_vec, &
+        mode_lv_hdd)
     !
     ! Thick-restart Lanczos diagonalization in real(kwf)
     !
@@ -60,35 +57,38 @@ contains
     !   output  eval(n_eig) : eigenvalues
     !           evec(ndim, n_eig) : eigenvectors
     interface 
-       subroutine matvec(v1, v2)
-         ! v2 = mat * v1
-         use wavefunction, only: type_vec_p
-         type(type_vec_p), intent(inout)  :: v1
-         type(type_vec_p), intent(inout) :: v2
-       end subroutine matvec
-       subroutine dotprod(v1, v2, r)
-         ! r = v1 * v2
-         use wavefunction, only: type_vec_p
-         type(type_vec_p), intent(in)  :: v1
-         type(type_vec_p), intent(in) :: v2
-         real(8), intent(out) :: r
-       end subroutine dotprod
-       subroutine matvec_jj(v1, v2)
-         ! v2 = mat * v1   for JJ 
-         use wavefunction, only: type_vec_p
-         type(type_vec_p), intent(inout)  :: v1
-         type(type_vec_p), intent(inout) :: v2
-       end subroutine matvec_jj
+        subroutine matvec(v1, v2)
+            ! v2 = mat * v1
+            use wavefunction, only: type_vec_p
+            type(type_vec_p), intent(inout)  :: v1
+            type(type_vec_p), intent(inout) :: v2
+        end subroutine matvec
+
+        subroutine dotprod(v1, v2, r)
+            ! r = v1 * v2
+            use wavefunction, only: type_vec_p
+            type(type_vec_p), intent(in)  :: v1
+            type(type_vec_p), intent(in) :: v2
+            real(8), intent(out) :: r
+        end subroutine dotprod
+
+        subroutine matvec_jj(v1, v2)
+            ! v2 = mat * v1   for JJ 
+            use wavefunction, only: type_vec_p
+            type(type_vec_p), intent(inout)  :: v1
+            type(type_vec_p), intent(inout) :: v2
+        end subroutine matvec_jj
     end interface
+
     real(8), intent(out) :: eval(:)
     type(type_vec_p), intent(inout) :: evec(:)
     integer(kdim), intent(in) :: ndim
     integer, intent(in), optional :: n_eig, maxiter, n_restart_vec, &
-         max_lanc_vec, mode_lv_hdd
+        max_lanc_vec, mode_lv_hdd
     real(8), intent(in), optional :: tol, eval_jj
     logical, intent(in), optional :: is_inner, is_load_snapshot
     integer :: neig, miter, iv, itr, n_itr, i, j, n, n_iv, n_res_vec, max_lv, &
-         n_iv_start, itr_a, mod_lv_hdd
+        n_iv_start, itr_a, mod_lv_hdd
     real(8) :: tl, an, bn, r, x, t
     logical :: is_load_s, is_time_exceed
     type(type_vec_p), allocatable :: vec(:)
@@ -97,7 +97,6 @@ contains
     character(len=12) :: c_inout
     integer(kdim) :: mq
     character(len=maxchar) :: fn_lv, c_num
-
 
     neig = min(size(eval), size(evec))
     if (present(n_eig)) neig = n_eig
@@ -115,11 +114,11 @@ contains
     if (present(max_lanc_vec)) max_lv = max_lanc_vec
     write(c_num,'(i0)') myrank
     if (present(is_inner)) then
-       c_inout = "          JJ"
-       fn_lv = trim(fn_base_lv)//"J_"//trim(c_num)//"_"
+        c_inout = "          JJ"
+        fn_lv = trim(fn_base_lv)//"J_"//trim(c_num)//"_"
     else
-       c_inout = "H "
-       fn_lv = trim(fn_base_lv)//"H_"//trim(c_num)//"_"
+        c_inout = "H "
+        fn_lv = trim(fn_base_lv)//"H_"//trim(c_num)//"_"
     end if
     is_time_exceed = .false.
     if (n_res_vec > max_lv)  stop "error: n_restart_vec > max_lanc_vec"
@@ -129,17 +128,17 @@ contains
     nskip_diag = 1
     nskip_time = 20
     if ((.not. present(is_inner)) .and. &
-         abs(sum_rule)<=1.d-8 .and. mod_lv_hdd/=2) then
-       if (max_lv > 500 ) nskip_diag = 5
-       if (max_lv > 1000) nskip_diag = 30
-       if (max_lv > 2000) nskip_diag = 50
-       if (nskip_diag /= 1) nskip_time = nskip_diag * 5
-       if (myrank == 0 .and. nskip_diag /= 1) write(*,'(a,i5)') &
+        abs(sum_rule) <= 1.d-8 .and. mod_lv_hdd /= 2) then
+        if (max_lv > 500 ) nskip_diag = 5
+        if (max_lv > 1000) nskip_diag = 30
+        if (max_lv > 2000) nskip_diag = 50
+        if (nskip_diag /= 1) nskip_time = nskip_diag * 5
+        if (myrank == 0 .and. nskip_diag /= 1) write(*,'(a,i5)') &
             "# of steps to skip to diagonalize Krylov subspace", nskip_diag
     end if
     
-    allocate( vec(max_lv), tmat(max_lv, max_lv), &
-         tevec(max_lv, max_lv), teval(max_lv), te_last(neig) )
+    allocate( vec(max_lv), tmat(max_lv, max_lv), tevec(max_lv, max_lv), &
+        teval(max_lv), te_last(neig) )
     
     tmat(:,:) = 0.d0
     teval(:) = 1.d6
@@ -150,22 +149,18 @@ contains
     is_load_s = .false.
     if (present(is_load_snapshot)) is_load_s = is_load_snapshot
     if (is_load_s) then
-
-       call load_snapshot(n_iv_start)
-       
+        call load_snapshot(n_iv_start)
+    
     else if (.not. associated(evec(1)%p)) then
-
-       stop "ERROR : Lanczos  initial vector"
-
+        stop "ERROR : Lanczos  initial vector"
+    
     else  ! initial vector = evec(1)
+        n_iv_start = 1
+        vec(1)%p => evec(1)%p
+        nullify( evec(1)%p )
 
-       n_iv_start = 1
-       vec(1)%p => evec(1)%p
-       nullify( evec(1)%p )
-
-       if (.not. present(is_inner) .and. present(eval_jj)) &
+        if (.not. present(is_inner) .and. present(eval_jj)) &
             call jj_refine( vec(1), 1, x )
-
     end if
 
     do iv = 1, size(evec)
@@ -174,153 +169,148 @@ contains
 
 
     outer: do itr = 1, miter
-       do iv = n_iv_start, max_lv-1
-          call allocate_l_vec( vec(iv+1)%p, ndim )
-          call matvec(vec(iv), vec(iv+1))
-          call dotprod(vec(iv+1), vec(iv), an)
-          tmat(iv, iv) = an
-          te_last(:neig) = teval(:neig)
-          if (mod_lv_hdd == 1) then 
-             call write_vec(iv, fn_lv, vec(iv))
-             call deallocate_l_vec( vec(iv)%p )
-          elseif (mod_lv_hdd == 2) then
-             if (iv > 2) then
-                if ( associated(vec(iv-2)%p) ) &
-                     call deallocate_l_vec( vec(iv-2)%p )
-             end if
-          end if
-          
- 
-          if (present(is_inner) .or. mod(iv-1, nskip_diag)==0 .or. iv==max_lv-1) then
-             call start_stopwatch(time_diag)
-             call diagonalize(tmat(:iv,:iv), teval(:iv), n_eig=neig)
-             call stop_stopwatch(time_diag, time_last=t)
-             if (myrank==0 .and. mod(iv-1, nskip_time)==0 .and. .not. present(is_inner)) &
-                  write(*,'(a,f10.3,a)') "time diag",t," sec"
-             if (present(is_inner) .and. .not. is_first_inner .and. &
-                  ! abs(teval(1) - eval_jj) > 0.5d0) then 
-                  abs(teval(1) - eval_jj) > 20d0) then
-                if (myrank==0) write(*,*) &
-                     "possible JJ collapse : check bn converge condition, try is_addrand", &
-                     teval(1), eval_jj
-                stop "possible JJ collapse "
-             end if
-          end if
-          
-          call reorth(iv+1)
-          call dotprod(vec(iv+1), vec(iv+1), bn)
+        do iv = n_iv_start, max_lv-1
+            call allocate_l_vec( vec(iv+1)%p, ndim )
+            call matvec(vec(iv), vec(iv+1))
+            call dotprod(vec(iv+1), vec(iv), an)
+            tmat(iv, iv) = an
+            te_last(:neig) = teval(:neig)
 
-          n_iv = iv
-          if ( (kwf==4 .and. abs(bn) < max(tl**2, 1.d-5)) .or. &
-!               (kwf==8 .and. abs(bn) < max(tl**2, 1.d-8))  ) then
-               (kwf==8 .and. abs(bn) < max(tl**2, 1.d-7))  ) then
-             if (myrank==0) write(*,'(2a,i5,20e10.2)') &
-                  trim(c_inout)," bn converged", n_iv, bn
-             exit outer
-          end if
-
-          bn = sqrt(bn)
-          tmat(iv, iv+1) = bn
-          tmat(iv+1, iv) = bn
-          
-          x = 1.d0/bn
-          !$omp parallel do private (mq)
-          do mq = 1, ndim
-             vec(iv+1)%p(mq) = x * vec(iv+1)%p(mq)
-          end do
-          if (.not. present(is_inner) .and. present(eval_jj)) then
-             call jj_refine( vec(iv+1), iv+1, x )
-             if (abs(eval_jj-x) > 0.1) then
-                if (myrank==0) write(*,*) " JJ_refine  not converged"
-                exit outer
-             end if
-          end if
-
-          if (present(is_inner)) then
-             if (teval(1)-eval_jj < tl &
-                  .or. abs(teval(1)-te_last(1)) < tl )  then 
-                if (myrank==0) write(*,'(2a,1f11.6, 2e14.6)') trim(c_inout), &
-                     " converged", teval(1), teval(1)-eval_jj
-                exit outer
-             end if
-             if (myrank==0) write(*,'(2a,2i5,10000f15.6)') &
-                  trim(c_inout),"  lanczos ", itr_a, iv, teval(:neig)
-          else
-
-             if (myrank==0 .and. abs(sum_rule)>1.d-8) then
-                call start_stopwatch(time_diag)
-                call diagonalize(tmat(:iv,:iv), teval(:iv), tevec(:iv,:iv))
-                call stop_stopwatch(time_diag)
-                do i = 1, iv
-                   !write(*,'(a,2i5,f12.5,f15.8)') "strength function", &
-                   !     iv, i,teval(i), sum_rule*tevec(1,i)**2
-                   write(*,'(a,2i5,f12.5,f15.8,f10.7)') "strength function", &
-                        iv, i,teval(i), sum_rule*tevec(1,i)**2, tevec(1,i)**2
-                end do
-             end if
-             
-             if (mod(iv-1, nskip_diag)==0 .or. iv == max_lv-1) then
-                if ( maxval( abs(teval(:neig)-te_last(:neig))) < tl )  then 
-                   if (myrank==0) write(*,'(2a,10000e14.6)') trim(c_inout),&
-                        " converged", te_last(:neig)-teval(:neig)
-                   exit outer
+            if (mod_lv_hdd == 1) then
+                call write_vec(iv, fn_lv, vec(iv))
+                call deallocate_l_vec( vec(iv)%p )
+            elseif (mod_lv_hdd == 2) then
+                if (iv > 2) then
+                    if ( associated(vec(iv-2)%p) ) &
+                        call deallocate_l_vec( vec(iv-2)%p )
                 end if
-                if (myrank==0) write(*,'(2a,2i5,10000f15.6)') &
-                     trim(c_inout),"  lanczos ", itr_a, iv, teval(:neig)
-             end if
-          end if
-          itr_a = itr_a + 1
+            end if
 
-!          if (.not. present(is_inner) .and. mod_lv_hdd == 2 &
-!               .and. mod(iv,20) == 0 ) call dump_snapshot(iv+1)
+            if (present(is_inner) .or. mod(iv - 1, nskip_diag) == 0 .or. iv == max_lv - 1) then
+                call start_stopwatch(time_diag)
+                call diagonalize(tmat(:iv,:iv), teval(:iv), n_eig=neig)
+                call stop_stopwatch(time_diag, time_last=t)
+                if (myrank == 0 .and. mod(iv - 1, nskip_time) == 0 .and. .not. present(is_inner)) &
+                    write(*,'(a,f10.3,a)') "time diag", t, " sec"
+                if (present(is_inner) .and. .not. is_first_inner .and. &
+                    ! abs(teval(1) - eval_jj) > 0.5d0) then 
+                    abs(teval(1) - eval_jj) > 20d0) then
+                    if (myrank == 0) write(*,*) &
+                        "possible JJ collapse : check bn converge condition, try is_addrand", &
+                        teval(1), eval_jj
+                    stop "possible JJ collapse "
+                end if
+            end if
+          
+            call reorth(iv + 1)
+            call dotprod(vec(iv + 1), vec(iv + 1), bn)
 
-       end do
+            n_iv = iv
+            if ( (kwf==4 .and. abs(bn) < max(tl**2, 1.d-5)) .or. &
+                ! (kwf==8 .and. abs(bn) < max(tl**2, 1.d-8))  ) then
+                (kwf==8 .and. abs(bn) < max(tl**2, 1.d-7))  ) then
+                if (myrank==0) write(*,'(2a,i5,20e10.2)') &
+                    trim(c_inout)," bn converged", n_iv, bn
+                exit outer
+            end if
 
-       n_itr = itr
-       if (itr==miter) exit
+            bn = sqrt(bn)
+            tmat(iv, iv + 1) = bn
+            tmat(iv + 1, iv) = bn
+          
+            x = 1.d0/bn
+            !$omp parallel do private (mq)
+            do mq = 1, ndim
+                vec(iv + 1)%p(mq) = x*vec(iv + 1)%p(mq)
+            end do
+            if (.not. present(is_inner) .and. present(eval_jj)) then
+                call jj_refine( vec(iv+1), iv+1, x )
+                if (abs(eval_jj-x) > 0.1) then
+                    if (myrank == 0) write(*,*) " JJ_refine  not converged"
+                    exit outer
+                end if
+            end if
 
-       call set_restart_vec(n_res_vec, n_iv)
+            if (present(is_inner)) then
+                if (teval(1) - eval_jj < tl &
+                    .or. abs(teval(1)-te_last(1)) < tl )  then 
+                    if (myrank == 0) write(*,'(2a,1f11.6, 2e14.6)') trim(c_inout), &
+                        " converged", teval(1), teval(1) - eval_jj
+                    exit outer
+                end if
+                if (myrank == 0) write(*,'(2a,2i5,10000f15.6)') &
+                    trim(c_inout),"  lanczos ", itr_a, iv, teval(:neig)
+            else
+                if (myrank == 0 .and. abs(sum_rule) > 1.d-8) then
+                    call start_stopwatch(time_diag)
+                    call diagonalize(tmat(:iv,:iv), teval(:iv), tevec(:iv,:iv))
+                    call stop_stopwatch(time_diag)
+                    do i = 1, iv
+                        ! write(*,'(a,2i5,f12.5,f15.8)') "strength function", &
+                        !     iv, i,teval(i), sum_rule*tevec(1,i)**2
+                        write(*,'(a,2i5,f12.5,f15.8,f10.7)') "strength function", &
+                            iv, i,teval(i), sum_rule*tevec(1,i)**2, tevec(1,i)**2
+                    end do
+                end if
+             
+                if (mod(iv - 1, nskip_diag) == 0 .or. iv == max_lv - 1) then
+                    if ( maxval( abs(teval(:neig) - te_last(:neig))) < tl )  then 
+                        if (myrank == 0) write(*,'(2a,10000e14.6)') trim(c_inout), &
+                            " converged", te_last(:neig) - teval(:neig)
+                        exit outer
+                    end if
+                    if (myrank == 0) write(*,'(2a,2i5,10000f15.6)') &
+                        trim(c_inout),"  lanczos ", itr_a, iv, teval(:neig)
+                end if
+            end if
+            itr_a = itr_a + 1
+            ! if (.not. present(is_inner) .and. mod_lv_hdd == 2 &
+            !     .and. mod(iv,20) == 0 ) call dump_snapshot(iv+1)
+        end do
 
-!       if (.not. present(is_inner) ) call dump_snapshot(n_res_vec+1)
-       n_iv_start = n_res_vec + 1
+        n_itr = itr
+        if (itr == miter) exit
+        call set_restart_vec(n_res_vec, n_iv)
+        ! if (.not. present(is_inner) ) call dump_snapshot(n_res_vec+1)
+        n_iv_start = n_res_vec + 1
     end do outer
 
-    if (myrank==0 .and. n_itr==miter) write(*,'(2a,20e10.2)') &
-         trim(c_inout)," not converged"
+    if (myrank == 0 .and. n_itr == miter) write(*,'(2a,20e10.2)') &
+        trim(c_inout)," not converged"
     is_first_inner = .false.    
 
-    if (mod_lv_hdd==2) then
-    ! if (mod_lv_hdd==2 .or. &
-    !      (.not.present(is_inner) .and. abs(sum_rule)>1.d-8)) then
-       call my_mpi_finalize()
-       if (myrank==0) write(*,*) " finished strength function"
-       stop
+    if (mod_lv_hdd == 2) then
+    ! if (mod_lv_hdd == 2 .or. &
+    !     (.not.present(is_inner) .and. abs(sum_rule)>1.d-8)) then
+        call my_mpi_finalize()
+        if (myrank == 0) write(*,*) " finished strength function"
+        stop
     end if
 
     if (neig > n_iv) neig = n_iv
     call set_restart_vec(neig, n_iv)
-!    if (.not. present(is_inner) ) call dump_snapshot(neig+1)
+    ! if (.not. present(is_inner) ) call dump_snapshot(neig+1)
     eval = 0.d0 
     eval(:neig) = teval(:neig)
-    call deallocate_l_vec( vec(neig+1)%p )
-    if (mod_lv_hdd==1) then
-       do i = 1, neig
-          call allocate_l_vec( evec(i)%p, ndim )
-          call read_vec( i, fn_lv, evec(i) )
-       end do
-    elseif (mod_lv_hdd==0) then
-       do i = 1, neig
-          evec(i)%p => vec(i)%p
-       end do
+    call deallocate_l_vec( vec(neig + 1)%p )
+    
+    if (mod_lv_hdd == 1) then
+        do i = 1, neig
+            call allocate_l_vec( evec(i)%p, ndim )
+            call read_vec( i, fn_lv, evec(i) )
+        end do
+    elseif (mod_lv_hdd == 0) then
+        do i = 1, neig
+            evec(i)%p => vec(i)%p
+        end do
     end if
 
-!    call print_max_l_vec()
-
+    ! call print_max_l_vec()
 
     if (is_time_exceed) then
-       call my_mpi_finalize()
-       if (myrank==0) write(*,*) "*** time limit closed ***"
-       stop
+        call my_mpi_finalize()
+        if (myrank == 0) write(*,*) "*** time limit closed ***"
+        stop
     end if
 
   contains
